6faedd2b0b7b9f9c033114400dc9d95b2807de2f
Saketh <saketh.ch20@iiits.in>
2024-06-19 00:13:17 +0530

    added basic header

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import logo from "./logo.png";
 
-const heading = React.createElement("h1", { id: "heading" }, "Hello World from React");
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={logo} alt="CRAVIO Logo" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
-
+root.render(<AppLayout />);
